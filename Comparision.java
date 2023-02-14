dc3495846968544e0c860c6afff7a8fba2a9e841
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-14 18:03:45 +0530

    using compareto method to compare 2 lengths

--- Comparision.java
+++ Comparision.java
@@ -31,23 +31,32 @@ public class Comparision {
 		System.out.println("enter Line-2 y2 value:");
 		int L2y2=s.nextInt();
 		
-		double firstLineLength = Math.sqrt((Math.pow(L1x2-L1x1,2))+(Math.pow(L1y2-L1y1,2)));
+		int firstLineLength = (int)Math.sqrt((Math.pow(L1x2-L1x1,2))+(Math.pow(L1y2-L1y1,2)));
 		System.out.println("Length of a line-1:"+firstLineLength);
 		
-		double secondLineLength = Math.sqrt((Math.pow(L2x2 - L2x1,2))+(Math.pow(L2y2 - L2y1,2)));
+		int secondLineLength = (int) Math.sqrt((Math.pow(L2x2 - L2x1,2))+(Math.pow(L2y2 - L2y1,2)));
 		System.out.println("Length of a line-2:"+secondLineLength);
 		
+		Comparision c = new Comparision();
+		c.compareTo(firstLineLength,secondLineLength);
+		c.equals(firstLineLength, secondLineLength);
+	}	
+		public void compareTo(int firstLineLength, int secondLineLength) {
 		
-		if ( firstLineLength > secondLineLength) {
-			System.out.println("first Line is bigger");
-		}else if (firstLineLength < secondLineLength ){
-			System.out.println("Second line is bigger");
-		}else {
-			System.out.println("Both are equal");
+		System.out.println("comparision of two lines:"+Integer.compare((firstLineLength),
+				(secondLineLength)));
+	   
+	}
+		
+		public void equals(int firstLineLength, int secondLineLength) {
+			
+			if(firstLineLength > secondLineLength) {
+				System.out.println("first line bigger");
+			}else if(firstLineLength < secondLineLength) {
+				System.out.println("second line bigger");
+			}else {
+				System.out.println("both lines are equal");
+			}
 		}
 		
-		//System.out.println(firstLineLength.compareto(secondLineLength);--> cannot use compareto method 
-	    s.close();
-	}
-
 }

68897a2b9504d4071e8269669aa3a2cbcb33a046
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-17 17:41:26 +0530

    check Equality of 2 lengths

--- Comparision.java
+++ Comparision.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.lang.Math;
 public class Comparision {
 
-	public static void main(String[] args) {
+	public static void main(String[] args, int firstLineLength, int secondLineLength) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		System.out.println("enter Line-1 x1 value:");
@@ -31,21 +31,16 @@ public class Comparision {
 		System.out.println("enter Line-2 y2 value:");
 		int L2y2=s.nextInt();
 		
-		double firstLineLength = Math.sqrt((Math.pow(L1x2-L1x1,2))+(Math.pow(L1y2-L1y1,2)));
-		System.out.println("Length of a line-1:"+firstLineLength);
+		Comparision c = new Comparision();
+		c.equals(firstLineLength,secondLineLength);
 		
-		double secondLineLength = Math.sqrt((Math.pow(L2x2 - L2x1,2))+(Math.pow(L2y2 - L2y1,2)));
-		System.out.println("Length of a line-2:"+secondLineLength);
-		
-		
-		if (firstLineLength > secondLineLength) {
-			System.out.println("first Line is bigger");
-		}else if (firstLineLength < secondLineLength ){
-			System.out.println("Second line is bigger");
-		}else{
-			System.out.println("Both are equal");
-		}
-	    s.close();
 	}
-
+	public boolean equals(int firstLineLength , int  secondLineLength) {
+		if(firstLineLength == secondLineLength) {
+		return true;
+		}else {
+			return false;
+		}
+	
+	}
 }

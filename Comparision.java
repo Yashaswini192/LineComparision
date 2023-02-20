496ca4bf8f870f2a05089fc019f892d25de60669
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-20 19:28:15 +0530

    using compareto method to compare 2 lengths

--- Comparision.java
+++ Comparision.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 import java.lang.Math;
 public class Comparision {
 
-	public static void main(String[] args) {
+	
+	public static void main(String[] args, int firstLineLength, int secondLineLength) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		System.out.println("enter Line-1 x1 value:");
@@ -31,13 +32,14 @@ public class Comparision {
 		System.out.println("enter Line-2 y2 value:");
 		int L2y2=s.nextInt();
 		
-		int firstLineLength = (int)Math.sqrt((Math.pow(L1x2-L1x1,2))+(Math.pow(L1y2-L1y1,2)));
-		System.out.println("Length of a line-1:"+firstLineLength);
+		double firstLineLength1 = (Math.sqrt((Math.pow(L1x2-L1x1,2))+(Math.pow(L1y2-L1y1,2))));
+		System.out.println("Length of a line-1:"+firstLineLength1);
 		
-		int secondLineLength = (int) Math.sqrt((Math.pow(L2x2 - L2x1,2))+(Math.pow(L2y2 - L2y1,2)));
-		System.out.println("Length of a line-2:"+secondLineLength);
+		double secondLineLength1 =  Math.sqrt((Math.pow(L2x2 - L2x1,2))+(Math.pow(L2y2 - L2y1,2)));
+		System.out.println("Length of a line-2:"+secondLineLength1);
 		
 		Comparision c = new Comparision();
+<<<<<<< Updated upstream
 		c.compareTo(firstLineLength,secondLineLength);
 		c.equals(firstLineLength, secondLineLength);
 	}	
@@ -58,5 +60,26 @@ public class Comparision {
 				System.out.println("both lines are equal");
 			}
 		}
+=======
+		c.compareTo(firstLineLength1,secondLineLength1);
+		c.equals(firstLineLength1, secondLineLength1);
+		s.close();
+	}	
+	
+	public boolean equals(double firstLineLength1,double secondLineLength1) {
+		if(firstLineLength1 == secondLineLength1) {
+			return true;
+			}
+		return false;
+	}
+	
+	public boolean compareTo(double firstLineLength1, double  secondLineLength1) {
+		if(firstLineLength1 > secondLineLength1) {
+		return true;
+		}else {
+			return false;
+		}
+	}
+>>>>>>> Stashed changes
 		
 }
